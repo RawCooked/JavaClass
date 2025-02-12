4ecbd27c83e99d1310fc83a11ff5ebfcae58a774
RawCooked <123646029+RawCooked@users.noreply.github.com>
2025-02-12 22:51:43 +0100

    haw el code

---
+++ ZooManagement/src/tn/esprit/gestionzoo/classes/Animal.java
@@ -0,0 +1,44 @@
+package tn.esprit.gestionzoo.classes;
+
+public class Animal {
+    private String family;
+    private String name;
+    private int age;
+    private boolean isMammal;
+
+    void setAge(int age){
+        if(age > 0){
+        this.age = age;
+        return;
+    }
+        System.out.println("age under 0, is it in conception ?");
+    }
+
+    String  getName(){
+        return name;
+    }
+
+    Animal()
+    {
+
+    }
+    public Animal(String family,String name,int age,boolean isMammal)
+    {
+        this.family=family;
+        this.name=name;
+        this.age=age;
+        this.isMammal=isMammal;
+    }
+    public void displayAnimal()
+    {
+        System.out.println("Family : " + family + "Name : " +name +"Age : " + age +
+        " Is Mammal : " + isMammal);
+    }
+
+    @Override
+    public String toString() {
+        return "Family : " + family + "Name : " +name +"Age : " + age +
+                " Is Mammal : " + isMammal;
+    }
+}
+
---
+++ ZooManagement/src/tn/esprit/gestionzoo/classes/Zoo.java
@@ -0,0 +1,98 @@
+package tn.esprit.gestionzoo.classes;
+
+public class Zoo {
+    private Animal[] animals;
+    private String name;
+    private String city;
+    private final int nbrCages=25;
+    private int compteurAnimal ;
+
+    public Zoo() {
+        compteurAnimal=0;
+        this.animals = new Animal[nbrCages];
+    }
+
+    void setName(String name) {
+        if (!name.isBlank()){
+            this.name = name;
+        }
+        else {
+            System.out.println("Le nom n'existe pas");
+        }
+    }
+
+
+    public Zoo(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public void displayZoo() {
+        System.out.println("Nom du tn.esprit.gestionzoo.classes.Zoo : " + name + " City : " + city + " Number de Cages : " + nbrCages);
+    }
+
+    @Override
+    public String toString() {
+        return "Nom du tn.esprit.gestionzoo.classes.Zoo : " + name + " City : " + city + " Number de Cages : " + nbrCages;
+    }
+
+    public boolean addAnimal(Animal a) {
+        if (searchAnimal(a) != -1) {
+            System.out.println("tn.esprit.gestionzoo.classes.Animal est exsitant");
+            return false;
+        }
+        if (!isZooFull()) {
+            animals[compteurAnimal] = a;
+            compteurAnimal++;
+            return true;
+        } else {
+            System.out.println("tn.esprit.gestionzoo.classes.Zoo est plein");
+            return false;
+        }
+    }
+
+
+    public void displayAnimals() {
+            System.out.println("Liste des animaux dans le zoo:");
+            for (int i = 0; i < compteurAnimal; i++) {
+                animals[i].displayAnimal();
+            }
+        }
+
+
+    public int searchAnimal(Animal a) {
+        String nom = a.getName();
+        for (int i = 0; i < compteurAnimal; i++) {
+            if (animals[i].getName().equals(nom)) {
+                return i; // Retourne l'index de l'animal
+            }
+        }
+        return -1;
+    }
+
+    public boolean removeAnimal(Animal a) {
+        int i = searchAnimal(a);
+        if (i != -1) {
+
+            for (int j = i; j < compteurAnimal - 1; j++) {
+                animals[j] = animals[j + 1];
+            }
+            animals[compteurAnimal - 1] = null;
+            compteurAnimal--;
+            return true;
+        }
+        return false;
+
+    }
+    public boolean  isZooFull()
+    {
+        return animals.length==nbrCages;
+    }
+    static  Zoo comparerZoo(Zoo z1, Zoo z2)
+    {
+        if(z1.compteurAnimal>=z2.compteurAnimal) {
+            return z1;
+        }
+        return z2;
+    }
+}
