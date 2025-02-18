a8b8ba75333f8357f1cfd8b8a2e67a47c7f11a46
RawCooked <123646029+RawCooked@users.noreply.github.com>
2025-02-18 10:27:13 +0100

    Update Animal.java
    corrected the getters and setters of animal

--- ZooManagement/src/tn/esprit/gestionzoo/classes/Animal.java
+++ ZooManagement/src/tn/esprit/gestionzoo/classes/Animal.java
@@ -11,7 +11,7 @@ package tn.esprit.gestionzoo.classes;
         if(age > 0){
         this.age = age;
         return;
-    }
+        }
         System.out.println("age under 0, is it in conception ?");
     }
 
@@ -22,13 +22,14 @@ package tn.esprit.gestionzoo.classes;
 
     public Animal()
     {
+
         System.out.println("Animal constructor");
     }
     public Animal(String family,String name,int age,boolean isMammal)
     {
         this.family=family;
         this.name=name;
-        this.age=age;
+        setAge(age);
         this.isMammal=isMammal;
     }
     public void displayAnimal()
